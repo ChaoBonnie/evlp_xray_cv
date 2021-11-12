c1a146b685643c63e154a835e72757447dcabbe2
ChaoBonnie <chaobonnie@hotmail.com>
2021-11-11 21:15:33 -0500

    - Kept binary labels as integer to adhere to new PyTorch API

--- tasks/binary_classification.py
+++ tasks/binary_classification.py
@@ -43,7 +43,7 @@ class BinaryClassificationTask(pl.LightningModule):
         loss = F.binary_cross_entropy_with_logits(y_pred, y_true)
 
         self.log('train_loss', loss)
-        self.train_accuracy(y_pred, y_true)
+        self.train_accuracy(F.sigmoid(y_pred), y_true)
         self.log('train_accuracy', self.train_accuracy, prog_bar=True)
 
         return loss
@@ -52,10 +52,10 @@ class BinaryClassificationTask(pl.LightningModule):
         x, y_true = batch
         y_pred = self.forward(x)
 
-        loss = F.cross_entropy(y_pred, y_true)
+        loss = F.binary_cross_entropy_with_logits(y_pred, y_true)
 
         self.log('val_loss', loss, prog_bar=True)
-        self.val_accuracy(y_pred, y_true)
+        self.val_accuracy(F.sigmoid(y_pred), y_true)
         self.log('val_accuracy', self.val_accuracy, prog_bar=True)
 
     def configure_optimizers(self):
