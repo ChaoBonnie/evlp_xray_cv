789523616b0358ec8977213f1e73a1258b8a72e3
ChaoBonnie <chaobonnie@hotmail.com>
2022-01-26 11:23:40 -0500

    - Removed module installation, added directory changes

--- cluster_scripts/pretrain_binary.sh
+++ cluster_scripts/pretrain_binary.sh
@@ -2,12 +2,11 @@
 #SBATCH --account=rrg-wanglab
 #SBATCH --gres=gpu:1       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=6  # Cores proportional to GPUs: 6 on Cedar, 16 on Graham.
-#SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
+#SBATCH --mem=16000M       # Memory proportional to GPUs: 32000 Cedar, 64000 Graham.
 #SBATCH --time=03-03:00
 #SBATCH --output=%N-%j.out
 
+cd ~/projects/rrg-wanglab/bonchao
 source imaging_env/bin/activate
-pip install torch --no-index
-pip install pytorch_lightning --no-index
-pip install torchvision --no-index
+cd evlp_xray_cv
 python pretrain_nihcxr.py --data_dir ~/scratch/NIH_images_512p --name first_attempt --binary --debug
